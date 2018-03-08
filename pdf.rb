2731da40b914d5abfd53c5a5f8c17443bf851986
Castulo J. Martinez <castulojavier@gmail.com>
2018-03-08 01:02:32 -0800

    working commit

--- pdf.rb
+++ pdf.rb
@@ -9,29 +9,36 @@ OptionParser.new do |opt|
   opt.on('--output Final_PDF') { |o| options[:output] = o }
 end.parse!
 
-# read the pdf
-initial_pdf = Magick::Image::read(options[:input]) { self.quality = 100 }
+# convert PDF to JPG images
+system( "pdftoppm #{options[:input]} #{options[:output]} -jpeg -gray -r 300 " )
+images = Dir["#{options[:output]}-*.jpg"].sort
 
-# convert all pages to 300 dpi in resolution and grayscale
-pdf_pages = Array.new
-index = 0
-initial_pdf.each do |page|
-  #page.density = '300'
-  #page.trim!
-  #page = page.sharpen(0, 3.0)
-  #grey_page = page.quantize(number_colors=256, colorspace=Magick::GRAYColorspace)
-  page = grey_page.write("#{index}.png") { self.quality = 100 }
-  pdf_pages << "#{index}.png"
-  index += 1
+# Get the size of all the files combined 
+size = 0
+images.each do |page|
+  size = size + File.size(page).to_f / 1000000
 end
 
-# write the final pdf
-final_pdf = Magick::ImageList.new(*pdf_pages) { self.quality = 100 }
-size_kb = final_pdf.filesize.to_f / 1000
-puts "File size: #{size_kb} Kb"
-final_pdf.write(options[:output]) { self.quality = 100 }
+# if the size is no bigger than 3 MB then just write them to PDF
+compression = Magick::JPEGCompression
+image_list = Magick::ImageList.new(*images)
+if size < 3 then
 
-# delete the temporary files
-#pdf_pages.each do |pdf|
-#  File.delete(pdf)
-#end
+  image_list.write("#{options[:output]}.pdf")
+  
+# if the size is bigger then first compress them by saving them as JPG again
+else
+  
+  image_list.each_with_index do |page, i|
+    page.write("#{options[:output]}-#{i + 1}.jpg") { self.quality = 10 }
+  end
+  images = Dir["#{options[:output]}-*.jpg"].sort
+  image_list = Magick::ImageList.new(*images)
+  image_list.write("#{options[:output]}.pdf")
+
+end 
+
+# delete the temporary images
+images.each do |png|
+  File.delete(png)
+end
