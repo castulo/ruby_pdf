23707fd4a417a9414394c28772c468ad716848c4
Castulo J. Martinez <castulojavier@gmail.com>
2018-03-08 11:13:50 -0800

    Adjust quality to be smaller than 3MB
    The generated pages(images) will be decreased in quality by 10%
    until they reach the desired size of < 3MB, or until the quality
    is 10%.

--- pdf.rb
+++ pdf.rb
@@ -2,41 +2,45 @@
 require 'rmagick'
 require 'optparse'
 
+# Parse options from command line
 options = {}
 OptionParser.new do |opt|
   opt.banner = "Usage: pdf.rb [options]"
   opt.on('--input Initial_PDF') { |o| options[:input] = o }
   opt.on('--output Final_PDF') { |o| options[:output] = o }
 end.parse!
+options[:output].slice! ".pdf"
 
 # convert PDF to JPG images
 system( "pdftoppm #{options[:input]} #{options[:output]} -jpeg -gray -r 300 " )
-images = Dir["#{options[:output]}-*.jpg"].sort
 
-# Get the size of all the files combined 
-size = 0
-images.each do |page|
-  size = size + File.size(page).to_f / 1000000
+quality = 100
+loop do 
+  
+  images = Dir["#{options[:output]}-*.jpg"].sort
+  image_list = Magick::ImageList.new(*images) 
+  
+  # Get the size of all the files combined 
+  size = 0
+  images.each do |page|
+    size = size + File.size(page).to_f / 1000000
+  end
+  
+  break if size < 3
+    
+  # if the size is bigger than 3MB, then first compress them by
+  # saving them as JPG again with lower quality
+  quality = quality > 10 ? quality - 10 : 10
+  image_list.each_with_index do |page, i|
+    page.write("#{options[:output]}-#{i + 1}.jpg") { self.quality = quality }
+  end
+    
 end
 
-# if the size is no bigger than 3 MB then just write them to PDF
-compression = Magick::JPEGCompression
-image_list = Magick::ImageList.new(*images)
-if size < 3 then
-
-  image_list.write("#{options[:output]}.pdf")
-  
-# if the size is bigger then first compress them by saving them as JPG again
-else
-  
-  image_list.each_with_index do |page, i|
-    page.write("#{options[:output]}-#{i + 1}.jpg") { self.quality = 10 }
-  end
-  images = Dir["#{options[:output]}-*.jpg"].sort
-  image_list = Magick::ImageList.new(*images)
-  image_list.write("#{options[:output]}.pdf")
-
-end 
+# write the final PDF
+images = Dir["#{options[:output]}-*.jpg"].sort
+image_list = Magick::ImageList.new(*images) 
+image_list.write("#{options[:output]}.pdf")
 
 # delete the temporary images
 images.each do |png|
